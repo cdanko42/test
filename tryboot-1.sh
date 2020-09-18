657f7199d6476176f46c7ab21f84aff0c56df723
Christopher Danko <christopher.p.danko-1@ou.edu>
2020-09-18 14:56:53 -0500

    lol

--- tryboot-1.sh
+++ tryboot-1.sh
@@ -5,11 +5,7 @@
 #SBATCH --mem=1024
 #SBATCH --output=regress_stdout.txt
 #SBATCH --error=regress_stderr.txt
-<<<<<<< HEAD
-#SBATCH --time=1:12:00:00
-=======
-#SBATCH --time=15:00:00
->>>>>>> 011ee7261d33b9a1d73edbb31f8c50ea8dbb80a1
+#SBATCH --time=01:12:00:00
 #SBATCH --job-name=simulations
 #SBATCH --mail-user=christopher.p.danko-1@ou.edu
 #SBATCH --mail-type=ALL
@@ -17,8 +13,4 @@
 #
 #################################################
 module load R
-<<<<<<< HEAD
-Rscript tryboot-1.R > out.txt
-=======
-Rscript tryboot.R > out.txt
->>>>>>> 011ee7261d33b9a1d73edbb31f8c50ea8dbb80a1
+Rscript tryboot-1.R > out-1.txt
